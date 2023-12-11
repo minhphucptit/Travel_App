1ecb67f78a0bdf9400e0e1e1d9a58f2aea2b9ef4
minhphucptit <nminhphuc99@gmail.com>
2023-12-11 19:53:37 +0700

    fix2

--- src/client/test/inputCheck.test.js
+++ src/client/test/inputCheck.test.js
@@ -1,6 +1,6 @@
 import { checkInput } from '../js/inputCheck.js';
 
-describe('Test Regex', function () {
+describe('Test Input', function () {
   it('Input should be a string', function () {
     const urlRGEX = /^[a-zA-Z\s]{0,255}$/;
     const urlTest = 'L0nd0n';
